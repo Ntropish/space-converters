54dcf41f9d12d8c4658fe31ff69f246ab9b5bd69
ntropish <ntropish@gmail.com>
2023-07-19 17:02:44 -0500

    Update exported types and add readme

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import Rect from "./Rect";
 import SpaceConverter from "./SpaceConverter";
 
+import { Span, Point } from "./index.d";
+
 export { Rect, SpaceConverter };
+export type { Span, Point };
