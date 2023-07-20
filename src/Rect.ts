4dcc0d9005190d9355c2750b57d1de3507f155fe
ntropish <ntropish@gmail.com>
2023-07-20 05:49:50 -0500

    Upgrade Rect to be compatible with DOMRect

--- src/Rect.ts
+++ src/Rect.ts
@@ -1,9 +1,36 @@
+/**
+ * Rect class, an alternative to DOMRect, representing a rectangle with x, y coordinates and dimensions.
+ * This class mimics the properties of a DOMRect and includes getters for top, right, bottom, and left,
+ * making it compatible and interchangeable with a DOMRect in most cases.
+ */
 class Rect {
+  /**
+   * The x-coordinate of the rectangle.
+   */
   x: number;
+
+  /**
+   * The y-coordinate of the rectangle.
+   */
   y: number;
+
+  /**
+   * The width of the rectangle.
+   */
   width: number;
+
+  /**
+   * The height of the rectangle.
+   */
   height: number;
 
+  /**
+   * Create a new rectangle.
+   * @param {number} x - The x-coordinate of the rectangle (default is 0).
+   * @param {number} y - The y-coordinate of the rectangle (default is 0).
+   * @param {number} width - The width of the rectangle (default is 0).
+   * @param {number} height - The height of the rectangle (default is 0).
+   */
   constructor(
     x: number = 0,
     y: number = 0,
@@ -14,6 +41,38 @@ class Rect {
     this.y = y;
     this.width = width;
     this.height = height;
+  }
+
+  /**
+   * Get the top value (same as y-coordinate) of the rectangle.
+   * @return {number} The top value of the rectangle.
+   */
+  get top(): number {
+    return this.y;
+  }
+
+  /**
+   * Get the right value of the rectangle.
+   * @return {number} The right value (x-coordinate plus width) of the rectangle.
+   */
+  get right(): number {
+    return this.x + this.width;
+  }
+
+  /**
+   * Get the bottom value of the rectangle.
+   * @return {number} The bottom value (y-coordinate plus height) of the rectangle.
+   */
+  get bottom(): number {
+    return this.y + this.height;
+  }
+
+  /**
+   * Get the left value (same as x-coordinate) of the rectangle.
+   * @return {number} The left value of the rectangle.
+   */
+  get left(): number {
+    return this.x;
   }
 }
 
